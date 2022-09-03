9f0bc831b5066f4db6d1ba3393c24335c046803a
Atikul-Islam <atikmina25@gmail.com>
2022-09-04 02:03:03 +0600

    added news categories

--- js/newsday.js
+++ js/newsday.js
@@ -1,4 +1,17 @@
-
-fetch('https://jsonplaceholder.typicode.com/todos/1')
+const loadCategory = () =>{ fetch('https://openapi.programming-hero.com/api/news/categories')
   .then(response => response.json())
-  .then(json => console.log(json))
+  .then(data => displayCategories(data.data.news_category))
+}
+
+const displayCategories = categories =>{
+        const categoriesContainer = document.getElementById('categories-container');
+        for(const category of categories){
+            const categoryDiv = document.createElement('div');
+            categoryDiv.innerHTML = `
+            <a>${category.category_name}</a>
+            `;
+            categoriesContainer.appendChild(categoryDiv);
+        }
+}
+
+loadCategory();
